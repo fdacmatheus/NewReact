97d7f0b30d1a2dd9c74f5e740077dc91a2d0df8d
Matheus Ferreira <matheus.fdac@gmail.com>
2021-08-10 15:03:03 -0300

    Alterações feitas dentro de APPjs
    Adicionado uma aprendizagem onde consigo usar JSX, aplicando a lógica  JSproxima a linguagem de marcação de texto

--- src/App.js
+++ src/App.js
@@ -1,9 +1,45 @@
 import React from 'react';
 
 const App = () => {
+  const luana = {
+    cliente: 'Luana',
+    idade: 27,
+    compras: [
+      { nome: 'Notebook', preco: 'R$ 2500' },
+      { nome: 'Geladeira', preco: 'R$ 3000' },
+      { nome: 'Smartphone', preco: 'R$ 1500' },
+    ],
+    ativa: true,
+  };
+
+  const mario = {
+    cliente: 'Mario',
+    idade: 31,
+    compras: [
+      { nome: 'Notebook', preco: 'R$ 2500' },
+      { nome: 'Geladeira', preco: 'R$ 3000' },
+      { nome: 'Smartphone', preco: 'R$ 1500' },
+      { nome: 'Guitarra', preco: 'R$ 3500' },
+    ],
+    ativa: false,
+  };
+  const dados = mario;
+
+  const somatotal = dados.compras
+    .map((item) => Number(item.preco.replace('R$ ', '')))
+    .reduce((a, b) => a + b);
   return (
     <div>
-      <h1>Meu app</h1>
+      <p>Nome: {dados.cliente}</p>
+      <p>Idade: {dados.idade}</p>
+      <p>
+        Situação:{' '}
+        <span style={{ color: dados.ativa ? 'green' : 'red' }}>
+          {dados.ativa ? 'Ativa' : 'Inativa'}
+        </span>
+      </p>
+      <p>Total Gasto: {somatotal}</p>
+      <p>{somatotal > 10000 ? 'Você está gastando mais de 10000' : ''}</p>
     </div>
   );
 };
