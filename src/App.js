510eb7fc766575062e510c48697e379af9d09d01
Matheus Ferreira <matheus.fdac@gmail.com>
2021-08-12 15:01:43 -0300

    Desafio JSX

--- src/App.js
+++ src/App.js
@@ -1,46 +1,46 @@
 import React from 'react';
 
+const produtos = [
+  {
+    id: 1,
+    nome: 'Smartphone',
+    preco: 'R$ 2000',
+    cores: ['#29d8d5', '#252a34', '#fc3766'],
+  },
+  {
+    id: 2,
+    nome: 'Notebook',
+    preco: 'R$ 3000',
+    cores: ['#ffd045', '#d4394b', '#f37c59'],
+  },
+  {
+    id: 3,
+    nome: 'Tablet',
+    preco: 'R$ 1500',
+    cores: ['#365069', '#47c1c8', '#f95786'],
+  },
+];
+
 const App = () => {
-  const luana = {
-    cliente: 'Luana',
-    idade: 27,
-    compras: [
-      { nome: 'Notebook', preco: 'R$ 2500' },
-      { nome: 'Geladeira', preco: 'R$ 3000' },
-      { nome: 'Smartphone', preco: 'R$ 1500' },
-    ],
-    ativa: true,
-  };
-
-  const mario = {
-    cliente: 'Mario',
-    idade: 31,
-    compras: [
-      { nome: 'Notebook', preco: 'R$ 2500' },
-      { nome: 'Geladeira', preco: 'R$ 3000' },
-      { nome: 'Smartphone', preco: 'R$ 1500' },
-      { nome: 'Guitarra', preco: 'R$ 3500' },
-    ],
-    ativa: false,
-  };
-  const dados = mario;
-
-  const somatotal = dados.compras
-    .map((item) => Number(item.preco.replace('R$ ', '')))
-    .reduce((a, b) => a + b);
+  const dados = produtos.filter(
+    ({ preco }) => Number(preco.replace('R$', '')) > 1500,
+  );
   return (
-    <div>
-      <p>Nome: {dados.cliente}</p>
-      <p>Idade: {dados.idade}</p>
-      <p>
-        Situação:{' '}
-        <span style={{ color: dados.ativa ? 'green' : 'red' }}>
-          {dados.ativa ? 'Ativa' : 'Inativa'}
-        </span>
-      </p>
-      <p>Total Gasto: {somatotal}</p>
-      <p>{somatotal > 10000 ? 'Você está gastando mais de 10000' : ''}</p>
-    </div>
+    <section>
+      {dados.map(({ id, nome, preco, cores }) => (
+        <div key={id}>
+          <h1>{nome}</h1>
+          <p>Preço: {preco}</p>
+          <ul>
+            {cores.map((cor) => (
+              <li style={{ backgroundColor: cor, color: 'white' }} key={cor}>
+                {cor}
+              </li>
+            ))}
+          </ul>
+        </div>
+      ))}
+    </section>
   );
 };
 
